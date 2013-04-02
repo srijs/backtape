404522caf20feb20cb1f0146cb69b9de709fd510
Sam Rijs <sam@dev.local>
2013-04-02 20:57:01 +0200

    First working version.

---
+++ app.js
@@ -0,0 +1,75 @@
+function Stepper(doms) {
+  var counter = 0;
+  return function step () {
+    var steps = arguments, dom, fn;
+    if (counter < steps.length) {
+      dom = doms[counter];
+      fn = steps[counter];
+      if (typeof fn === 'function') {
+        console.log('FUN');
+        dom.style.display = 'block';
+        dom.className = 'processing';
+        var next = function (c, msg) {
+          dom.className = c;
+          dom.innerHTML = msg;
+          counter++;
+          if (c !== 'error') step.apply(null, steps);
+        };
+        fn(function (msg) { next('ready', msg); },
+           function (msg) { next('error', msg); });
+      }
+    }
+  };
+}
+
+Doctape({
+  appType: 'client',
+  appId: '7b1fe099-b090-4ee2-996b-f83e56aee70f',
+  callbackURL: 'http://localhost:5000/',
+  scope: ['file.read', 'account']
+}).run(function () {
+  var dt = this,
+      remoteDocs = null,
+      newDocs = null;
+  Stepper(document.querySelectorAll('#steps div'))(
+    function (done, error) {
+      dt.getAccount(null, function (err, account) {
+        if (err) { return error('Could not connect to doctape.'); }
+        document.getElementById('headline').innerHTML += ' ~' + account.username;
+        done('Connected and logged in.');
+      });
+    },
+    function (done, error) {
+      dt.getDocumentList(null, function (err, docs) {
+        if (err) { return error('Could not read remote documents.'); }
+        remoteDocs = docs;
+        done('Read <b>'+ Object.keys(docs).length +' remote</b> documents.');
+      });
+    },
+    function (done, error) {
+      var diffReq = new XMLHttpRequest();
+      diffReq.open('POST', '/diff');
+      diffReq.setRequestHeader('Content-Type', 'application/json');
+      diffReq.send(JSON.stringify(remoteDocs));
+      diffReq.addEventListener('error', function (ev) {
+        error('Error comparing documents.');
+      }, false);
+      diffReq.addEventListener('load', function (ev) {
+        newDocs = JSON.parse(ev.target.responseText);
+        done('Found <b>'+ Object.keys(newDocs).length +' new</b> documents.');
+      }, false);
+    },
+    function (done, error) {
+      var syncReq = new XMLHttpRequest();
+      syncReq.open('POST', '/sync?token=' + dt.core.getValidAccessToken());
+      syncReq.setRequestHeader('Content-Type', 'application/json');
+      syncReq.send(JSON.stringify(newDocs));
+      syncReq.addEventListener('error', function (ev) {
+        error('Error syncing documents.');
+      }, false);
+      syncReq.addEventListener('load', function (ev) {
+        done('Synced documents.');
+      }, false);
+    }
+  );
+});
---
+++ server.js
@@ -0,0 +1,66 @@
+var fs = require('fs'),
+    https = require('https'),
+    express = require('express');
+
+var app = express();
+
+app.configure(function () {
+  app.use(express.bodyParser());
+  app.use('/', express.static(__dirname));
+});
+
+app.post('/diff', function (req, res) {
+
+  try { fs.mkdirSync('./Backtape'); } catch (e) {;}
+
+  fs.readdir('./Backtape', function (err, files) {
+    var rDocs = req.body;
+    if (err) {
+      res.send(500);
+    } else {
+      files.forEach(function (file) {
+        if (typeof rDocs[file] !== 'undefined') {
+          delete rDocs[file];
+        }
+      });
+      res.json(rDocs);
+    }
+  });
+
+});
+
+app.post('/sync', function (req, res) {
+
+  var token = req.param('token'),
+      docs = req.body,
+      keys = Object.keys(docs);
+
+  !function sync () {
+    if (keys.length > 0) {
+      var key = keys.shift();
+      https.get('https://api.doctape.com/v1/doc/' + key + '/original?access_token=' + token, function (r) {
+        var w = fs.createWriteStream('./Backtape/.' + key, {encoding: 'utf8'});
+        if (r.statusCode === 200) {
+          r.pipe(w, {end: false});
+          r.on('end', function () {
+            w.end();
+            fs.rename('./Backtape/.' + key, './Backtape/' + key, function (err) {
+              if (err) {
+                res.send(500);
+              } else {
+                sync();
+              }
+            });
+          });
+        } else {
+          res.send(500);
+        }
+      });
+    } else {
+      res.send(200);
+    }
+  }();
+
+});
+
+app.listen(5000);
